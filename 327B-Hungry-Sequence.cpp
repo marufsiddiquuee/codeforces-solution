11b762fbef0205dc56efb452abeeb2ff67eaad4d
Maruf Siddique <c253085@ugrad.iiuc.ac.bd>
2026-08-16 19:50:46 +0600

    Add Codeforces 327B - Hungry Sequence solution

---
+++ 327B-Hungry-Sequence.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<vector>
+#include<algorithm>
+#include<string>
+#include<cmath>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n; 
+    
+    int num=1000000;
+    
+    while(n--)
+    {
+    cout << num <<" ";
+    num++;
+    }
+    
+    }
